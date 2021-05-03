63212de6ffc30a30078a1d413588e7cc7e8dc577
Steve Hearnden <steve@hearnden.org.uk>
2021-05-03 11:16:38 +0100

    Fix for UMC202HD

--- 03-jamulus/files/runjack.sh
+++ 03-jamulus/files/runjack.sh
@@ -2,6 +2,8 @@
 echo runjack.sh
 if aplay -l | grep 'USB Audio CODEC' ; then
     /usr/bin/jackd -P75 -dalsa -r48000 -p64 -n16 -Chw:CODEC -Phw:CODEC,0
+elif aplay -l | grep 'UMC202HD 192k' ; then
+    /usr/bin/jackd -P75 -dalsa -r48000 -p64 -n16 -Chw:U192k -Phw:U192k,0
 else
     /usr/bin/jackd -P75 -dalsa -r48000 -p64 -n16 -Chw:sndrpihifiberry -Phw:sndrpihifiberry,0
 fi
